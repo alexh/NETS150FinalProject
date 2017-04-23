4a0538367120c4284e9dd720c4b1a2c0af5065b7
Henry Zhu <henryzhu@seas.upenn.edu>
2017-04-23 15:25:21 -0400

    Graphing visualizer works

--- NETS150HW5/src/Main.java
+++ NETS150HW5/src/Main.java
@@ -1,9 +1,17 @@
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 		FileParser fp = new FileParser("../test.txt");
-
+		Graph graph = new Graph();
+		graph.init();
+	      JFrame frame = new JFrame("MyPanel");
+	      frame.getContentPane().add(graph);
+	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      frame.pack();
+	      frame.setLocationRelativeTo(null);
+	      frame.setVisible(true);
 	}
 
 }
